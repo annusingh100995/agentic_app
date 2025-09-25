5adb5ed37d012ea2eee11d80a48785d082bc8cbb
annusingh100995 <annusingh100995@gmail.com>
2025-09-25 14:49:28 +0200

    add auth file

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Depends, Request
 from pydantic import BaseModel
 from manager import run_agent_by_intent
 from logger_config import logger
+from auth import verify_api_key
 import uvicorn
 
 app = FastAPI()
@@ -9,7 +10,7 @@ app = FastAPI()
 class RequestBody(BaseModel):
     query: str
 
-# Logging middleware
+# Middleware for logging
 @app.middleware("http")
 async def log_requests(request: Request, call_next):
     logger.info(f"Incoming request: {request.method} {request.url}")
@@ -18,16 +19,11 @@ async def log_requests(request: Request, call_next):
     return response
 
 @app.post("/master")
-async def master_api(body: RequestBody):
+async def master_api(body: RequestBody, x_api_key : str = Depends(verify_api_key)):
     logger.info(f"Received query: {body.query}")
     result = await run_agent_by_intent(body.query)
     logger.info(f"Master API returning result: {result}")
     return {"result": result}
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=True
-    )
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
